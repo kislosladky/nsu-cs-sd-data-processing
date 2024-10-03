7369e9f93e41315e96ad05f677d28e57ccf292e7
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-10-03 14:01:00 +0700

    fix

--- JavaConcurrency/Task3/src/main/java/ru/nsu/kislitsyn/task3/Main.java
+++ JavaConcurrency/Task3/src/main/java/ru/nsu/kislitsyn/task3/Main.java
@@ -20,18 +20,18 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        String[] toPrint = {"first word", "first thread"};
-        String[][] changes = {{"second word", "second child"},
+        String[] stringGroups = {"first word", "first thread"};
+        String[][] stringChanges = {{"second word", "second child"},
                 {"third word", "third child"},
                 {"forth word", "forth child"}};
 
         for (int i = 0; i < 4; i++) {
 
-            Child child = new Child(toPrint);
+            Child child = new Child(stringGroups);
             child.start();
 
             if (i < 3) {
-                toPrint = changes[i];
+                stringGroups = stringChanges[i];
             }
         }
     }
