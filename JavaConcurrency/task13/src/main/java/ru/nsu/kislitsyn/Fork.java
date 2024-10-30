2e35719069ca70728686df3117d3d7e745445b53
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-10-31 01:23:51 +0700

    task13

---
+++ JavaConcurrency/task13/src/main/java/ru/nsu/kislitsyn/Fork.java
@@ -0,0 +1,37 @@
+package ru.nsu.kislitsyn;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Fork {
+    private final int id;
+    private final Lock lock = new ReentrantLock();
+
+    public Fork (int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void lock() {
+        lock.lock();
+        System.out.println("Fork " + id + " is locked");
+    }
+
+    public void unlock() {
+        lock.unlock();
+        System.out.println("Fork " + id + " is free");
+    }
+
+    public boolean tryLock() {
+        boolean result = lock.tryLock();
+        if (result) {
+            System.out.println("Fork " + id + " was not unlocked yet");
+        } else {
+            System.out.println("Fork " + id + " is locked");
+        }
+        return result;
+    }
+}
