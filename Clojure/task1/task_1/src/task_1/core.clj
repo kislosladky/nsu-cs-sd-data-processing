05e7b45f6e5a44d415fc5fa3fc68b1e4633f9221
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-11-14 15:09:52 +0700

    added comments

--- Clojure/task1/task_1/src/task_1/core.clj
+++ Clojure/task1/task_1/src/task_1/core.clj
@@ -1,17 +1,22 @@
 (ns task-1.core)
 
+; добавляю символ в начало каждого слова
 (defn add-char [char words]
   (map (fn [word] (str char word)) words))
 
+; отбрасываю слова, у которых 2 первых символа совпадают
 (defn filter-duplicates [words]
   (remove (fn [word] (= (first word) (second word))) words))
 
+; объединяю подсписки в один список
 (defn add-alphabet [words alphabet]
   (reduce concat (map (fn [x] (add-char x words)) alphabet)))
 
+; последовательно применяю 2 функции, описанных выше
 (defn add-n-filter [words alphabet]
   (filter-duplicates (add-alphabet words alphabet)))
 
+; полное решение. Использую reduce вместо цикла и игнорирую значения, по которым идет итерация
 (defn all-strings [alphabet length]
   (reduce
      (fn [words _] (add-n-filter words alphabet))
